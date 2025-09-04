20d3df3b700e8173c096313982ed347f121ad86a
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-09-03 22:00:14 -0300

    feat: Generate code for Riverpod providers and JSON serialization
    This commit introduces auto-generated files for Riverpod state management providers and JSON serialization/deserialization logic.
    
    Key changes:
    
    - **`lib/data/models/user_model.g.dart`**: Added generated code for `UserModel` using `json_serializable`. This includes `_$UserModelFromJson` and `_$UserModelToJson` methods for converting between `UserModel` instances and JSON objects.
    - **`lib/presentation/providers/auth_provider.g.dart`**: Added generated code for the `AuthProvider` using `riverpod_generator`. This defines `authProvider` as an `AutoDisposeNotifierProvider` for managing authentication state.
    - **`lib/presentation/providers/nfc_provider.g.dart`**: Added generated code for the `NfcProvider` (referred to as `Nfc` in the generator) using `riverpod_generator`. This defines `nfcProvider` as an `AutoDisposeNotifierProvider` for managing NFC status and operations.
    - **`lib/presentation/providers/supabase_auth_provider.g.dart`**: Added generated code for the `SupabaseAuthProvider` (referred to as `SupabaseAuth` in the generator) using `riverpod_generator`. This defines `supabaseAuthProvider` as an `AutoDisposeNotifierProvider` for managing Supabase-specific authentication state.

---
+++ lib/presentation/providers/supabase_auth_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'supabase_auth_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$supabaseAuthHash() => r'c00659bcaebf41fde81726cc0925b04a0fbab96b';
+
+/// See also [SupabaseAuth].
+@ProviderFor(SupabaseAuth)
+final supabaseAuthProvider =
+    AutoDisposeNotifierProvider<SupabaseAuth, AsyncValue<UserEntity?>>.internal(
+      SupabaseAuth.new,
+      name: r'supabaseAuthProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$supabaseAuthHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SupabaseAuth = AutoDisposeNotifier<AsyncValue<UserEntity?>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
