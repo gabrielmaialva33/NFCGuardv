c57595f5f18ce82495c37d7f772b38a9e22da083
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-09-03 22:41:01 -0300

    test: Add missing import in brazilian_validation_test.dart
    This commit adds the missing import for `UserEntity` in the `brazilian_validation_test.dart` file.
    
    Key changes:
    - Added `import 'package:nfc_guard/domain/entities/user_entity.dart';` to `test/integration/brazilian_validation_test.dart`.

--- test/integration/brazilian_validation_test.dart
+++ test/integration/brazilian_validation_test.dart
@@ -1,6 +1,7 @@
 import 'package:all_validations_br/all_validations_br.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nfc_guard/core/constants/app_constants.dart';
+import 'package:nfc_guard/domain/entities/user_entity.dart';
 import 'package:search_cep/search_cep.dart';
 
 /// Integration tests for Brazilian validation features
