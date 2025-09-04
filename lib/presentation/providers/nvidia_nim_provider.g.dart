58c3a00b25c2dfe46eb0668fb5c7bdc4594b6d54
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-09-03 22:58:45 -0300

    feat: Generate Riverpod code for Nvidia NIM provider
    This commit introduces auto-generated files for the `NvidiaNimService` and `NvidiaNimState` Riverpod providers.
    
    Key changes:
    - **`lib/presentation/providers/nvidia_nim_provider.g.dart`**: Added generated code using `riverpod_generator`.
        - Defines `nvidiaNimServiceProvider` as an `AutoDisposeProvider<NvidiaNimService>`.
        - Defines `nvidiaNimStateProvider` as an `AutoDisposeNotifierProvider<NvidiaNimState, AsyncValue<String>>`.

---
+++ lib/presentation/providers/nvidia_nim_provider.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'nvidia_nim_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$nvidiaNimServiceHash() => r'e51f2b8dec52896906c64411b926268b91a67861';
+
+/// See also [nvidiaNimService].
+@ProviderFor(nvidiaNimService)
+final nvidiaNimServiceProvider = AutoDisposeProvider<NvidiaNimService>.internal(
+  nvidiaNimService,
+  name: r'nvidiaNimServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$nvidiaNimServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef NvidiaNimServiceRef = AutoDisposeProviderRef<NvidiaNimService>;
+String _$nvidiaNimStateHash() => r'3d02daa65ce4c9128a586a5b09370f3198191dc4';
+
+/// See also [NvidiaNimState].
+@ProviderFor(NvidiaNimState)
+final nvidiaNimStateProvider =
+    AutoDisposeNotifierProvider<NvidiaNimState, AsyncValue<String>>.internal(
+      NvidiaNimState.new,
+      name: r'nvidiaNimStateProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$nvidiaNimStateHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$NvidiaNimState = AutoDisposeNotifier<AsyncValue<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
